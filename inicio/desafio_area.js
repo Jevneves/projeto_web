5fdded9ce3bf53853d09ce7983e48923a1e724cb
Jeverson Neves <jevneves@gmail.com>
2021-09-23 16:36:05 -0300

    Desafio Área Circunferência

---
+++ inicio/desafio_area.js
@@ -0,0 +1,13 @@
+// PI * raio * raio
+
+const PI = 3.141592;
+let raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log('Resolução: Área da Circunferência')
+console.log('O resultado da área é ' + areaCirc + 'm².')
+
+console.log(Math.PI)
+areaCirc = Math.PI * raio * raio;
+console.log('O resultado da área é ' + areaCirc + 'm².')
+
